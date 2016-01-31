b9cdc7c2e86368a417c96ebb599224e982917aa0
higgs241 <nathanielostrer@gmail.com>
2016-01-31 13:56:08 -0500

    yo

--- powerful-atlas-707/appengine_config.py
+++ powerful-atlas-707/appengine_config.py
@@ -1,7 +1,3 @@
-#from google.appengine.ext import vendor
+from google.appengine.ext import vendor
 
-<<<<<<< HEAD
-#vendor.add('lib')
-=======
 vendor.add('libs')
->>>>>>> 679c66e391222830448359cbbf922895d3be546f
