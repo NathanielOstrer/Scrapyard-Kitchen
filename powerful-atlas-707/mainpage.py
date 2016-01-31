b39ba906d9ed123889d176ab904cb25be301f891
mnara <c16mn>
2016-01-31 10:38:53 -0500

    added css and changed mainpage

--- powerful-atlas-707/mainpage.py
+++ powerful-atlas-707/mainpage.py
@@ -2,25 +2,31 @@ def render(data):
 	message = "This is the main page!"
 
 	lines = [
-	"<html>",
-
-	"<center>",
-
-	"<h1>",
-	
-	'<p style="font-family: signpainter; font-size: 250%"> Scrapyard Kitchen </p>',
-	
-	"</h1>",
-
-	'<form action="/search">',
-
-	'<input type="text" name=ingredients>',
-
-	"</form>",
-
-	"</center>",
-
-	"</html>"
+    "<html>",
+    "<head>",
+    '<link rel="stylesheet" href="./TrinHackStyle.css">',
+    "<title>Scrapyard Kitchen</title>",
+    "</head>",
+    '<body class="bground" style="background-image: url(bground.jpg);">',
+    '<span class="backbox">',
+    '<input type="text" placeholder="add ingredient" id="ingred" autocomplete="on">',
+    "<input type='button' onclick='changeText2()' value='Add' class='button'/>",
+    "</span>",
+    '<span class="list">',
+    "Ingredients:",
+    '<ol class="ingredient_list" id="demo"></ol>',
+    "</span>",
+    "<script>",
+    "var list = document.getElementById('demo');",
+    "function changeText2() {",
+    "var ingred = document.getElementById('ingred').value;",
+    "var entry = document.createElement('li');",
+    "entry.appendChild(document.createTextNode(ingred));",
+    "list.appendChild(entry);",
+    "}",
+    "</script>",
+    "</body>",
+    "</html>",
 	]
 
 	return lines
